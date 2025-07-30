dd93c47af69fcbe05cc16a8b8ce8f46f8c0c4e47
Snorvor <bulezar@gmail.com>
2025-07-30 20:59:15 -0300

    the greater from three

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int theBigger;
+
+        first = first > second ? first : second;
+        first = first > third ? first : third;
+        theBigger = (second > third) ? second : third;
+        theBigger = theBigger > first ? theBigger : first;
+        System.out.println(theBigger);
     }
 }
