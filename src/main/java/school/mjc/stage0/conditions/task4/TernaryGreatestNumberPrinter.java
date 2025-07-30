b07092c5c8aff85544a5357598f33ddc6ec72feb
Snorvor <bulezar@gmail.com>
2025-07-30 20:16:40 -0300

    evaluate more bigger or equality of two numbers

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int result;
+
+        if (first > second) {
+            System.out.println(first);
+        } else if (second > first) {
+            System.out.println(second);
+        } else {
+            result = first == second ? first : second;
+            System.out.println(result);
+        }
     }
 }
