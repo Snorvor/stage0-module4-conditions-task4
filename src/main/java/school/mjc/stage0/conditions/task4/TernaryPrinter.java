fcc82016801d704f0ddd7e7448395f25cea6c2aa
Snorvor <bulezar@gmail.com>
2025-07-30 21:04:06 -0300

    the greater from two numbers

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String theBiggest;
+
+        theBiggest = first > second ? "first" : "second";
+
+        System.out.println(theBiggest);
     }
 }
